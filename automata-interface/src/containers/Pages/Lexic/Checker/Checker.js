14bf87543657dd10f5efba89bc91582a0f0cc462
AlanKev117 <fuentes.alan.98@gmail.com>
2019-02-18 19:21:44 -0600

    Más detalles a la interfaz

--- automata-interface/src/containers/Pages/Lexic/Checker/Checker.js
+++ automata-interface/src/containers/Pages/Lexic/Checker/Checker.js
@@ -3,19 +3,33 @@ import React from "react";
 import classes from "./Checker.module.css";
 
 const checker = props => {
-	const checks = props.automata.map(automaton => (
-		<label key={automaton.getName()} className={classes.container}>
-			{automaton.getName()}
-			<input type="checkbox" name={automaton.getName()} onChange={props.checked}/>
-			<span className={classes.checkmark} />
-		</label>
-	));
-	return (
-		<div className={classes.Checker}>
-			<label>{props.text}</label>
-			{checks}
-		</div>
-	);
+    const checks = props.automata.map(automaton => (
+        <div key={automaton.getName()}>
+            <label className={classes.container}>
+                {automaton.getName()}
+                <input
+                    type="checkbox"
+					name={automaton.getName()}
+					onChange={props.autoChecked}
+                />
+                <span className={classes.checkmark} />
+            </label>
+            <span>
+                <input
+                    type="number"
+                    onChange={props.tokenChanged}
+                    placeholder="Token"
+					name={automaton.getName()}
+                />
+            </span>
+        </div>
+    ));
+    return (
+        <div className={classes.Checker}>
+            <label>{props.text}</label>
+            {checks}
+        </div>
+    );
 };
 
 export default checker;
--- automata-interface/src/containers/Pages/Lexic/Lexic.js
+++ automata-interface/src/containers/Pages/Lexic/Lexic.js
@@ -5,79 +5,98 @@ import Checker from "./Checker/Checker";
 import classes from "./Lexic.module.css";
 
 class Lexic extends Component {
-	state = {
-		lexicName: "",
-		checkedAutomata: new Set()
-	};
+    state = {
+        lexicName: "",
+        checkedAutomata: new Set(),
+        tokens: {}
+    };
 
-	automatonCheckedHandler = event => {
-		const name = event.target.name;
-		const isChecked = event.target.checked;
-		this.setState(prevState => {
-			const newCheckedAutomata = prevState.checkedAutomata;
-			if (isChecked) {
-				newCheckedAutomata.add(name);
-			} else {
-				newCheckedAutomata.delete(name);
-			}
-			return { checkedAutomata: newCheckedAutomata };
-		});
-	};
+    automatonCheckedHandler = event => {
+        const name = event.target.name;
+        const isChecked = event.target.checked;
+        this.setState(prevState => {
+            const newCheckedAutomata = prevState.checkedAutomata;
+            if (isChecked) {
+                newCheckedAutomata.add(name);
+            } else {
+                newCheckedAutomata.delete(name);
+            }
+            return { checkedAutomata: newCheckedAutomata };
+        });
+    };
 
-	createLexicAnalizerHandler = () => {
-		const selectedAutos = this.props.automata
-			.filter(automaton =>
-				this.state.checkedAutomata.has(automaton.getName())
-			)
-			.map(selectedAuto => selectedAuto.copy());
+    tokenChangedHandler = event => {
+        const autoName = event.target.name;
+        const tokenNumber = event.target.value;
+        this.setState(prevState => {
+            const tokens = prevState.tokens;
+            tokens[autoName] = tokenNumber;
+            return { tokens: tokens };
+        });
+        console.log(this.state.tokens);
+    };
 
-		const newAnalizer = Misc.unirAFNAnalisis(
-			selectedAutos,
-			this.state.lexicName
+    createLexicAnalizerHandler = () => {
+        const selectedAutos = this.props.automata
+            .filter(automaton =>
+                this.state.checkedAutomata.has(automaton.getName())
+            )
+            .map(selectedAuto => selectedAuto.copy());
+
+        selectedAutos.forEach(auto => {
+            auto.setToken(this.state.tokens[auto.getName()]);
+        });
+
+        const newAnalizer = Misc.unirAFNAnalisis(
+            selectedAutos,
+            this.state.lexicName
 		);
-		this.props.automata.push(newAnalizer);
-	};
+		console.log(newAnalizer);
+        this.props.automata.push(newAnalizer);
+    };
 
-	lexicNameChangedHandler = event => {
-		const newName = event.target.value;
-		this.setState({
-			lexicName: newName
-		});
-	};
+    lexicNameChangedHandler = event => {
+        const newName = event.target.value;
+        this.setState({
+            lexicName: newName
+        });
+    };
 
-	render() {
-		return (
-			<main className={classes.Lexic}>
-				<h1>Analizador Léxico</h1>
+    render() {
+        return (
+            <main className={classes.Lexic}>
+                <h1>Analizador Léxico</h1>
 
-				{this.props.automata.length > 0 ? (
-					<div style={{ textAlign: "center" }}>
-						<Checker
-							text="Seleccione autómatas para formar el analizador léxico"
-							checked={this.automatonCheckedHandler}
-							automata={this.props.automata}
-						/>
-						{this.state.checkedAutomata.size > 0 ? (
-							<div style={{ textAlign: "center" }}>
-								<input
-									type="text"
-									value={this.state.lexicName}
-									onChange={this.lexicNameChangedHandler}
-								/>
-								<button
-									onClick={this.createLexicAnalizerHandler}
-								>
-									Unir para analizador léxico
-								</button>
-							</div>
-						) : null}
-					</div>
-				) : (
-					<h2>No hay autómatas disponibles</h2>
-				)}
-			</main>
-		);
-	}
+                {this.props.automata.length > 0 ? (
+                    <div style={{ textAlign: "center" }}>
+                        <Checker
+                            text="Seleccione autómatas para formar el analizador léxico"
+                            autoChecked={this.automatonCheckedHandler}
+                            tokenChanged={this.tokenChangedHandler}
+                            automata={this.props.automata}
+                        />
+                        {this.state.checkedAutomata.size > 0 ? (
+                            <div style={{ textAlign: "center" }}>
+                                <input
+                                    type="text"
+                                    value={this.state.lexicName}
+                                    onChange={this.lexicNameChangedHandler}
+                                    placeholder="Nombre del analizador"
+                                />
+                                <button
+                                    onClick={this.createLexicAnalizerHandler}
+                                >
+                                    Unir para analizador léxico
+                                </button>
+                            </div>
+                        ) : null}
+                    </div>
+                ) : (
+                    <h2>No hay autómatas disponibles</h2>
+                )}
+            </main>
+        );
+    }
 }
 
 export default Lexic;
