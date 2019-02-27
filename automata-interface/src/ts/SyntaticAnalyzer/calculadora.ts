54f7ff67e95fdbb50676ed87f873663fa9b85dc2
Rafael Garcia Garcia <Ralph2g@github.com>
2019-02-27 00:23:24 -0600

    declaracion clase y metodos

---
+++ automata-interface/src/ts/SyntaticAnalyzer/calculadora.ts
@@ -0,0 +1,46 @@
+import { Automaton } from "../Automaton/Automaton";
+import Misc from "../Misc/Misc";
+import { State } from "../State/State";
+import { Transition } from "../Transition/Transition";
+import { LexicAnalyzer } from "../LexicAnalizer/LexicAnalizer";
+
+class calculadora{
+
+
+    constructor(cadena: string, analizador: LexicAnalyzer){
+
+    }
+
+    G = () =>{
+
+    };
+
+    E = () =>{
+
+    };
+
+    Ep = () =>{
+
+    };
+
+    T = () =>{
+
+    };
+
+    Tp = () =>{
+
+    };
+    
+    p = () =>{
+
+    };
+
+    Pp = () =>{
+
+    };
+
+    F = () =>{
+        
+    }
+};
+export { calculadora };
