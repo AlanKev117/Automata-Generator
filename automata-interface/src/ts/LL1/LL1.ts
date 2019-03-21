21b37797b79596945fe6bbb3b61a72a5ea966063
Rafael Garcia Garcia <Ralph2g@github.com>
2019-03-20 20:33:40 -0600

    Se declaran algunas gramaticas nada importante

---
+++ automata-interface/src/ts/LL1/LL1.ts
@@ -0,0 +1,20 @@
+import { LexicAnalyzer } from "../LexicAnalyzer/LexicAnalyzer";
+import { Automaton } from "../Automaton/Automaton";
+import { Rule } from "../LL1/Rule";
+
+
+class Gramatica {
+	/**
+	 * Nombre que se le asignará al autómata cuando se cree.
+	 *
+	 * @private
+	 * @type {Set<rules}
+	 * @memberof Automaton
+	 */
+	private listaReglas: Set<Rule>;
+
+
+
+}
+
+export { Gramatica };
---
+++ automata-interface/src/ts/LL1/Rule.ts
@@ -0,0 +1,14 @@
+class Rule {
+	/**
+	 * Nombre que se le asignará al autómata cuando se cree.
+	 *
+	 * @private
+	 * @type {Set<rules}
+	 * @memberof Automaton
+	 */
+
+
+
+}
+
+export { Rule };
