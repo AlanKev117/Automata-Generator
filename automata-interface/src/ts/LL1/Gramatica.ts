493f750de91722ecb12cb18406859c98253d0e46
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2019-03-25 09:40:29 -0600

    Update Gramatica.ts

--- automata-interface/src/ts/LL1/Gramatica.ts
+++ automata-interface/src/ts/LL1/Gramatica.ts
@@ -1,13 +1,32 @@
 import { Node } from "./Node";
 
 class Gramatica {
-	private head: Node;
+    private head: Node;
 
-	constructor () {
-		this.head = null;
-	}
+    constructor() {
+        this.head = null;
+    }
 
-	
+    public readonly getRightSidesWith = (symbol: string) => {
+        const rightSides: Set<Node> = new Set<Node>();
+        for (
+            let currentHead = this.head;
+            currentHead != null;
+            currentHead = currentHead.down
+        ) {
+            for (
+                let currentNode = currentHead.right;
+                currentNode != null;
+                currentNode = currentNode.right
+            ) {
+                if (currentNode.symbol === symbol) {
+					rightSides.add(currentHead);
+					break;
+                }
+            }
+		}
+		return rightSides;
+    };
 }
 
 export { Gramatica };
