2f28e00f67fc33a2a04dc94578a90089368eeaa7
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2019-03-25 09:31:45 -0600

    Create LL1.ts

---
+++ automata-interface/src/ts/LL1/LL1.ts
@@ -0,0 +1,14 @@
+import { Gramatica } from "./Gramatica";
+
+class LL1 {
+    private G: Gramatica;
+    
+    public readonly first = () => {
+
+    }
+    public readonly follow = () => {
+
+    }
+}
+
+export { LL1 };
