e2c07af8ecb42c779040d8ec8b2a831e499bf5b3
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2019-03-25 09:27:46 -0600

    Gramática y Nodo

---
+++ automata-interface/src/ts/LL1/Gramatica.ts
@@ -0,0 +1,13 @@
+import { Node } from "./Node";
+
+class Gramatica {
+	private head: Node;
+
+	constructor () {
+		this.head = null;
+	}
+
+	
+}
+
+export { Gramatica };
---
+++ automata-interface/src/ts/LL1/Node.ts
@@ -0,0 +1,8 @@
+class Node {
+    private symbol: string;
+    private right: Node;
+    private down: Node;
+}
+
+
+export { Node }
