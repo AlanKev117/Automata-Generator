53569bc5820742f3fd25a1faa08a09823ae21458
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2019-05-11 19:01:23 -0500

    Correciones en el analizador de gramática de gramáticas.

--- automata-interface/src/ts/LL1/LL1.ts
+++ automata-interface/src/ts/LL1/LL1.ts
@@ -3,91 +3,117 @@ import { Gramatica } from "./Gramatica";
 import { Node } from "./Node";
 
 class LL1 {
-    private G: Gramatica; //lista de reglas
+	private G: Gramatica; //lista de reglas
 
-    constructor(G: Gramatica) {
-        this.G = G;
-    }
+	constructor(G: Gramatica) {
+		this.G = G;
+	}
 
-    public readonly first = (symbols: Array<string>) => {
-        //Declaramos conjunto vacio
-        let set = new Set<string>();
+	public readonly first = (symbols: Array<string>) => {
+		//Declaramos conjunto vacio
+		let set = new Set<string>();
 
-        // Verificamos si el símbolo recibido es un terminal o épsilon.
-        if (this.G.terminals.has(symbols[0]) || symbols[0] === Misc.EPSILON) {
-            set.add(symbols[0]);
-            return set;
-        }
+		// Verificamos si el símbolo recibido es un terminal o épsilon.
+		if (this.G.terminals.has(symbols[0]) || symbols[0] === Misc.EPSILON) {
+			set.add(symbols[0]);
+			return set;
+		}
 
-        // Si el símbolo es un no terminal, procedemos de la siguiente forma.
-        const rightSides = this.G.getRightSidesWith(symbols[0]);
-        for (let rightSide of rightSides) {
-            set = new Set<string>(
-                [...set].concat(...this.first(rightSide.split("")))
-            );
-        }
+		// Si el símbolo es un no terminal, procedemos de la siguiente forma.
+		const rightSides = this.G.getRightSidesWith(symbols[0]);
+		for (let rightSide of rightSides) {
+			set = new Set<string>(
+				[...set].concat(...this.first(rightSide.split("")))
+			);
+		}
 
-        if (set.has(Misc.EPSILON) && symbols.length != 1) {
-            set.delete("\u03B5");
-            symbols.shift();
-            set = new Set<string>([...set].concat(...this.first(symbols)));
-        }
+		if (set.has(Misc.EPSILON) && symbols.length != 1) {
+			set.delete("\u03B5");
+			symbols.shift();
+			set = new Set<string>([...set].concat(...this.first(symbols)));
+		}
 
-        return set;
-    };
+		return set;
+	};
 
-    public readonly follow = (symbol: string) => {
-        let set = new Set<string>();
-        if (symbol === this.G.startSymbol) {
-            set.add("$");
-        }
-        const gammas = this.G.getLeftSidesAndGammasWith(symbol);
-        for (let leftSide in gammas) {
-            if (gammas[leftSide].length !== 0) {
-                set = new Set<string>(
-                    [...set].concat(...this.first(gammas[leftSide]))
-                );
-                if (set.has(Misc.EPSILON)) {
-                    set.delete(Misc.EPSILON);
-                    set = new Set<string>(
-                        [...set].concat(...this.follow(leftSide))
-                    );
-                }
-            } else {
-                if (leftSide === symbol) {
-                    return new Set<string>();
-                }
-                set = new Set<string>(
-                    [...set].concat(...this.follow(leftSide))
-                );
-            }
-        }
+	public readonly follow = (symbol: string) => {
+		let set = new Set<string>();
+		if (symbol === this.G.startSymbol) {
+			set.add("$");
+		}
+		const gammas = this.G.getLeftSidesAndGammasWith(symbol);
+		for (let leftSide in gammas) {
+			if (gammas[leftSide].length !== 0) {
+				set = new Set<string>(
+					[...set].concat(...this.first(gammas[leftSide]))
+				);
+				if (set.has(Misc.EPSILON)) {
+					set.delete(Misc.EPSILON);
+					set = new Set<string>(
+						[...set].concat(...this.follow(leftSide))
+					);
+				}
+			} else {
+				if (leftSide === symbol) {
+					return new Set<string>();
+				}
+				set = new Set<string>(
+					[...set].concat(...this.follow(leftSide))
+				);
+			}
+		}
 
-        return set;
-    };
+		return set;
+	};
 
-    public createLL1Table() {
-        const head = this.G.rules;
-        const table = {};
-        for (
-            let leftSide: Node = this.G.rules, ruleIndex: number = 0;
-            leftSide != null;
-            leftSide = leftSide.down
-        ) {
-            const transSymbols = this.first([
-                ...this.G.rightSideToString(head)
-            ]);
-            transSymbols.forEach(symbol => {
-                if (!table[leftSide.symbol]) {
-                    table[leftSide.symbol] = {};
-                }
-                table[leftSide.symbol][symbol] = [
-                    this.G.rightSideToString(leftSide.right),
-                    ruleIndex++
-                ];
-            });
-        }
-    }
+	public createLL1Table() {
+		// const head = this.G.rules;
+		// const table = {};
+		// for (
+		//     let leftSide: Node = this.G.rules, ruleIndex: number = 0;
+		//     leftSide != null;
+		//     leftSide = leftSide.down
+		// ) {
+		//     const transSymbols = this.first([
+		//         ...this.G.rightSideToString(head)
+		//     ]);
+		//     transSymbols.forEach(symbol => {
+		//         if (!table[leftSide.symbol]) {
+		//             table[leftSide.symbol] = {};
+		//         }
+		//         table[leftSide.symbol][symbol] = [
+		//             this.G.rightSideToString(leftSide.right),
+		//             ruleIndex++
+		//         ];
+		//     });
+		// }
+		const head: Node = this.G.rules;
+	}
+
+	/**
+	 * Regresa un arreglo de nodos (reglas) donde los índices
+	 * identifican a cada regla pra la tabla LL1.
+	 *
+	 * @private
+	 * @memberof LL1
+	 * @param {Node} rules El nodo de las reglas de una gramática
+	 * @returns {Node[]}
+	 */
+	private enumerateRules = (rules: Node) => {
+		const enumRules: Node[] = [];
+		for (let aux = rules; aux != null; aux = aux.down) {
+			const current = new Node(aux.symbol);
+			//for (let )
+		}
+		return enumRules;
+	};
+
+	private getRulesFromBranch = (branch: Node) => {
+		const rules = [];
+		for (let aux = branch; aux != null; aux = aux.right) {
+			
+		}
+	};
 }
 
 export { LL1 };
--- automata-interface/src/ts/LL1/SyntaxAnalyzerGrammar.ts
+++ automata-interface/src/ts/LL1/SyntaxAnalyzerGrammar.ts
@@ -5,142 +5,145 @@ import { Gramatica } from "./Gramatica";
 import { Automaton } from "../Automaton/Automaton";
 
 enum Token {
-    ARROW = 1,
-    PIPE,
-    SEMICOLON,
-    SYMBOL
+	ARROW = 1,
+	PIPE,
+	SEMICOLON,
+	SYMBOL
 }
 
 class SyntaxAnalyzerGrammar {
-    private lexicAnalyzer: LexicAnalyzer;
+	private lexicAnalyzer: LexicAnalyzer;
 
-    constructor(input: string) {
-        const tokens = { ...Token };
-        const automata = this.createAutomataForLexic();
-        this.lexicAnalyzer = new LexicAnalyzer(
-            automata,
-            tokens,
-            "Analizador de Gramáticas",
-            input
-        );
-    }
+	constructor(input: string) {
+		const tokens = { ...Token };
+		const automata = this.createAutomataForLexic();
+		this.lexicAnalyzer = new LexicAnalyzer(
+			automata,
+			tokens,
+			"Analizador de Gramáticas",
+			input
+		);
+	}
 
-    private readonly createAutomataForLexic = () => {
-        const autoARROW: Automaton = new SyntaxAnalyzerRegex("\\-&>").solve(
-            "ARROW"
-        );
-        const autoPIPE: Automaton = new SyntaxAnalyzerRegex("\\|").solve(
-            "PIPE"
-        );
-        const autoSEMICOLON: Automaton = new SyntaxAnalyzerRegex(";").solve(
-            "SEMICOLON"
-        );
-        const autoSYMBOL: Automaton = new SyntaxAnalyzerRegex(
-            "[a-z]|[A-Z]|[0-9]|\\&"
-        ).solve("SYMBOL");
-        return [autoARROW, autoPIPE, autoSEMICOLON, autoSYMBOL];
-    };
+	private readonly createAutomataForLexic = () => {
+		const autoARROW: Automaton = new SyntaxAnalyzerRegex("\\-&>").solve(
+			"ARROW"
+		);
+		const autoPIPE: Automaton = new SyntaxAnalyzerRegex("\\|").solve(
+			"PIPE"
+		);
+		const autoSEMICOLON: Automaton = new SyntaxAnalyzerRegex(";").solve(
+			"SEMICOLON"
+		);
+		const autoSYMBOL: Automaton = new SyntaxAnalyzerRegex(
+			"[a-z]|[A-Z]|[0-9]|\\&"
+		).solve("SYMBOL");
+		return [autoARROW, autoPIPE, autoSEMICOLON, autoSYMBOL];
+	};
 
-    public solve = (nameOfNewGrammar: string) => {
-        const val: Node[] = [];
-        if (this.G(val)) {
-            const grammar = new Gramatica(nameOfNewGrammar);
-            grammar.rules = val[0];
-            grammar.setNonTerminals(val[0]);
-            grammar.setTerminals(val[0]);
-            grammar.startSymbol = val[0].symbol;
-            return grammar;
-        } else {
-            alert("Error sintáctico");
-            return null;
-        }
-    };
+	public solve = (nameOfNewGrammar: string) => {
+		const val: Node[] = [];
+		if (this.G(val)) {
+			const grammar = new Gramatica(nameOfNewGrammar);
+			grammar.rules = val[0];
+			grammar.setNonTerminals(val[0]);
+			grammar.setTerminals(val[0]);
+			grammar.startSymbol = val[0].symbol;
+			return grammar;
+		} else {
+			alert("Error sintáctico");
+			return null;
+		}
+	};
 
-    /**
-     * Función global de descenso recursivo.
-     *
-     * @memberof SyntaxAnalyzerGrammar
-     */
-    public readonly G = (node: Node[]) => {
-        return this._LR_(node);
-    };
+	/**
+	 * Función global de descenso recursivo.
+	 *
+	 * @memberof SyntaxAnalyzerGrammar
+	 */
+	public readonly G = (node: Node[]) => {
+		return this._LR_(node);
+	};
 
-    public readonly _LR_ = (node: Node[]) => {
-        if (this._R_(node)) {
-            if (this.lexicAnalyzer.getToken() === Token.SEMICOLON) {
-                // Nuevo nodo para guardar una regla.
-                let node1: Node[] = [];
-                // Estado del analizador léxico antes de entrar a _LR_ de nuevo.
-                let s: object = this.lexicAnalyzer.getLexicState();
-                if (this._LR_(node1)) {
-                    node[0].down = node1[0];
-                    return true;
-                }
-                this.lexicAnalyzer.setLexicState(s);
-                return true;
-            }
-            return false;
-        }
-    };
+	public readonly _LR_ = (node: Node[]) => {
+		if (this._R_(node)) {
+			if (this.lexicAnalyzer.getToken() === Token.SEMICOLON) {
+				// Nuevo nodo para guardar una regla.
+				let node1: Node[] = [];
+				// Estado del analizador léxico antes de entrar a _LR_ de nuevo.
+				let s: object = this.lexicAnalyzer.getLexicState();
+				if (this._LR_(node1)) {
+					node[0].down = node1[0];
+					return true;
+				}
+				this.lexicAnalyzer.setLexicState(s);
+				return true;
+			}
+			return false;
+		}
+	};
 
-    public readonly _R_ = (node: Node[]) => {
-        let token: number;
-        if (this._LI_(node)) {
-            token = this.lexicAnalyzer.getToken();
-            if (token === Token.ARROW) {
-                let node1: Node[] = [];
-                if (this._LLD_(node1)) {
-                    node[0].right = node1[0];
-                    return true;
-                }
-            }
-        }
-        return false;
-    };
-    public readonly _LI_ = (node: Node[]) => {
-        let token: number = this.lexicAnalyzer.getToken();
-        if (token === Token.SYMBOL) {
-            node[0] = new Node(this.lexicAnalyzer.getCurrentLexem());
-            return true;
-        }
-        return false;
-    };
-    public readonly _LLD_ = (node: Node[]) => {
-        let token: number;
-        if (this._LD_(node)) {
-            token = this.lexicAnalyzer.getToken();
-            if (token === Token.PIPE) {
-                if (this._LLD_(node)) {
-                    return true;
-                }
-                return false;
-            }
-            this.lexicAnalyzer.returnToken();
-            return true;
-        }
-        return false;
-    };
-    public readonly _LD_ = (node: Node[]) => {
-        return this._LS_(node);
-    };
-    public readonly _LS_ = (node: Node[]) => {
-        let token: number = this.lexicAnalyzer.getToken();
-        if (token === Token.SYMBOL) {
-            node[0] = new Node(this.lexicAnalyzer.getCurrentLexem());
-            if (this._LS_(node)) {
-                let s: object = this.lexicAnalyzer.getLexicState();
-                let node1: Node[] = null;
-                if (this._LS_(node1)) {
-                    node[0].right = node1[0];
-                    return true;
-                }
-                this.lexicAnalyzer.setLexicState(s);
-            }
-            return true;
-        }
-        this.lexicAnalyzer.returnToken();
-        return false;
-    };
+	public readonly _R_ = (node: Node[]) => {
+		let token: number;
+		if (this._LI_(node)) {
+			token = this.lexicAnalyzer.getToken();
+			if (token === Token.ARROW) {
+				let node1: Node[] = [];
+				if (this._LLD_(node1)) {
+					node[0].right = node1[0];
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+	public readonly _LI_ = (node: Node[]) => {
+		let token: number = this.lexicAnalyzer.getToken();
+		if (token === Token.SYMBOL) {
+			node[0] = new Node(this.lexicAnalyzer.getCurrentLexem());
+			return true;
+		}
+		return false;
+	};
+
+	public readonly _LLD_ = (node: Node[]) => {
+		let token: number;
+		if (this._LD_(node)) {
+			token = this.lexicAnalyzer.getToken();
+			if (token === Token.PIPE) {
+				const node1: Node[] = [];
+				if (this._LLD_(node1)) {
+					node[0].down = node[1];
+					return true;
+				}
+				return false;
+			}
+			this.lexicAnalyzer.returnToken();
+			return true;
+		}
+		return false;
+	};
+	public readonly _LD_ = (node: Node[]) => {
+		return this._LS_(node);
+	};
+	public readonly _LS_ = (node: Node[]) => {
+		let token: number = this.lexicAnalyzer.getToken();
+		if (token === Token.SYMBOL) {
+			node[0] = new Node(this.lexicAnalyzer.getCurrentLexem());
+			//if (this._LS_(node)) {
+			let s: object = this.lexicAnalyzer.getLexicState();
+			const node1: Node[] = [];
+			if (this._LS_(node1)) {
+				node[0].right = node1[0];
+				return true;
+			}
+			this.lexicAnalyzer.setLexicState(s);
+			//}
+			return true;
+		}
+		this.lexicAnalyzer.returnToken();
+		return false;
+	};
 }
 
 export { SyntaxAnalyzerGrammar };
