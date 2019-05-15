d0d706bd7d4be55a018b01bb893f6ff319705261
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2019-05-15 13:13:28 -0500

    Cerradura épsilon para LR1

---
+++ automata-interface/src/ts/Grammar/LR1/Item.ts
@@ -0,0 +1,17 @@
+class Item {
+    public rule: object;
+    public terminals: string[];
+
+    constructor(rule: object, terminals: string[]) {
+        const val = [...Object.values(rule)[0]];
+        const key = Object.keys(rule)[0];
+        this.rule[key] = val;
+        this.terminals = [...terminals];
+    }
+
+    public readonly equals = (item: Item) => {
+        return JSON.stringify(this) === JSON.stringify(item);
+    };
+}
+
+export { Item };
--- automata-interface/src/ts/Grammar/LR1/LR1.ts
+++ automata-interface/src/ts/Grammar/LR1/LR1.ts
@@ -1,6 +1,7 @@
 import Misc from "../../Misc/Misc";
 import { Gramatica } from "../Gramatica";
 import { Node } from "../Node";
+import { Item } from "./Item";
 
 class LR1 {
     private G: Gramatica;
@@ -10,7 +11,7 @@ class LR1 {
     constructor(G: Gramatica) {
         this.G = G;
         this.augmentGammar();
-        this.rulesToItems();
+        this.rulesToObject();
         this.LR1Table = this.createLR1Table();
     }
 
@@ -67,7 +68,7 @@ class LR1 {
      * @private
      * @memberof LR1
      */
-    private readonly rulesToItems = () => {
+    private readonly rulesToObject = () => {
         this.rules = {};
         for (
             let leftSide = this.G.rules;
@@ -76,7 +77,7 @@ class LR1 {
         ) {
             this.rules[leftSide.symbol] = [];
             for (let rule = leftSide.right; rule !== null; rule = rule.down) {
-                let ruleString = Misc.DOT;
+                let ruleString = "";
                 for (let aux = rule; aux !== null; aux = aux.right) {
                     ruleString += aux.symbol;
                 }
@@ -86,21 +87,115 @@ class LR1 {
     };
 
     /**
-     * Cerradura épsilon para las gramáticas LR1.
+     * Cerradura épsilon para las gramáticas LR1 de un conjunto de Items.
      *
      * @private
      * @memberof LR1
      */
-    private readonly epsilonClosure = (rules: Set<[object, Set<string>]>) => {
+    private readonly epsilonClosure = (rules: Item[]) => {
+
         // Se agregan al conjunto resultado las reglas que se pasan como argumento.
         // Tratamos al conjunto temporalmente como un arreglo para mayor rapidez
         // en las operaciones de lectura y escritura.
         const closure = [...rules];
 
-        //Agregamos el resto de reglas según el contenido de res.
-        for (let pair of closure) {
-            
+        //Agregamos el resto de reglas según el contenido de closure.
+        for (let item of closure) {
+            // Obtenemos el lado derecho de la regla del item.
+            const rightSide = Object.values(item.rule)[0];
+
+            // Obtenemos el índice del símbolo después del punto.
+            const afterDotIndex = rightSide.indexOf(Misc.DOT) + 1;
+
+            // Con él, obtenemos el símbolo después del punto.
+            const afterDotSymbol = rightSide[afterDotIndex];
+
+            // Si el símbolo (B) es no terminal,
+            if (this.G.nonTerminals.has(afterDotSymbol)) {
+                // Obtenemos las producciones crudas que genera B.
+                const productions = this.rules[afterDotSymbol];
+                // Por cada una,
+                for (let prod of productions) {
+                    // Se calcula FIRST del conjunto formado por los símbolos después de B
+                    // y los terminales del item.
+                    const first = this.firstLR1(
+                        [...rightSide.slice(afterDotIndex + 1)].concat(item.terminals)
+                    );
+
+                    // Para cada símbolo del FIRST,
+                    for (let symbol of first) {
+                        // Se genera un nuevo item.
+                        const newProd = {};
+                        newProd[afterDotSymbol] = Misc.DOT + prod;
+                        const newItem = new Item(newProd, [symbol]);
+
+                        // Si ese item no se encuentra en la cerradura, se agrega.
+                        if (!closure.find(_item => _item.equals(newItem))) {
+                            closure.push(newItem);
+                        }
+                    }
+                }
+            }
         }
+
+        return closure;
+    };
+
+    /**
+     * Obtiene el conjunto FIRST de un conjunto de símbolos.
+     *
+     * @private
+     * @memberof LR1
+     */
+    private readonly firstLR1 = (symbols: string[]) => {
+        /**
+         * Tratamos al set first como un arreglo para mantener el orden,
+         * que es necesario para la generación de la tabla LR1.
+         */
+        const set = [];
+
+        /**
+         * Se verifica el caso básico, que es cuando el primer símbolo es un terminal.
+         */
+        if (
+            this.G.terminals.has(symbols[0]) ||
+            symbols[0] === Misc.SAFE_EPSILON ||
+            symbols[0] === Misc.PESOS
+        ) {
+            set.push(symbols[0]);
+            return set;
+        }
+
+        /**
+         * En caso de que symbols[0] no sea terminal, se procede a obtener
+         * el FIRST de los lados derechos de dicho símbolo.
+         */
+        const rightSides = this.rules[symbols[0]];
+        for (let rightSide of rightSides) {
+            // Se unen los conjuntos.
+            const nextFirst = this.firstLR1([...rightSide]);
+            nextFirst.forEach(symbol => {
+                if (!set.includes(symbol)) {
+                    set.push(symbol);
+                }
+            });
+        }
+
+        /**
+         * Caso donde se incluya épsilon en el conjunto.
+         */
+        if (set.includes(Misc.SAFE_EPSILON) && symbols.length > 1) {
+            delete set[set.indexOf(Misc.SAFE_EPSILON)];
+            symbols.shift();
+            const nextFirst = this.firstLR1(symbols);
+            nextFirst.forEach(symbol => {
+                if (!set.includes(symbol)) {
+                    set.push(symbol);
+                }
+            });
+        }
+
+        return set;
     };
 
     /**
@@ -122,3 +217,5 @@ class LR1 {
         }
     };
 }
+
+export { LR1 };
