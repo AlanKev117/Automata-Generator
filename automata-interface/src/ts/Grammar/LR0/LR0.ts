739309623e9c7dcc6723cc67507a2995b0ba6ea5
Damian Alanis Ramirez <damian.alanis98@gmail.com>
2019-05-26 23:54:26 -0500

    First y Follow (aun no funcionan)

--- automata-interface/src/ts/Grammar/LR0/LR0.ts
+++ automata-interface/src/ts/Grammar/LR0/LR0.ts
@@ -109,10 +109,17 @@ class LR0{
 							JSON.stringify(endItem.rule)
 					)
 				);
-
-				for (const terminal of this.G.terminals ) {
+				//let terminalsFinal = new Array();
+				this.first("E");
+				for (const terminal of endItem.terminals ) {
 					// Si ya ha sido definida una operación shift en la casilla, se detiene el procedimiento.
-                    
+					
+					for(const item of terminal){
+						//if(item != Misc.PESOS) console.log("FOLLOW(" + item + ") = " + this.follow(item));
+						//console.log("FOLLOW(T)= " + this.follow("T"));
+						//terminalsFinal.push(this.follow(item));
+					}
+					//console.log("FOLLOW: " + terminalsFinal);	
                     if (row[terminal]) {
                         alert("La gramática no es LR0 \n" 
                         + "Introduzca una gramática no ambigua \n" 
@@ -412,6 +419,86 @@ class LR0{
 		}
 		item.rule[leftSide] = parts.join(Misc.DOT);
 	};
+/*
+    private readonly first = (symbols: Array<string>) => {
+        //Declaramos conjunto vacio
+        let set = new Set<string>();
+
+        // Verificamos si el símbolo recibido es un terminal o épsilon.
+        if (this.G.terminals.has(symbols[0]) || symbols[0] === Misc.EPSILON) {
+            set.add(symbols[0]);
+            return set;
+        }
+
+        // Si el símbolo es un no terminal, procedemos de la siguiente forma.
+        const rightSides = this.G.getRightSidesWith(symbols[0]);
+        for (let rightSide of rightSides) {
+            set = new Set<string>(
+                [...set].concat(...this.first(rightSide.split("")))
+            );
+        }
+
+        if (set.has(Misc.EPSILON) && symbols.length != 1) {
+            set.delete(Misc.EPSILON);
+            symbols.shift();
+            set = new Set<string>([...set].concat(...this.first(symbols)));
+        }
+
+        return set;
+    };
+
+*/
+	private readonly first = (symbol: string) => {
+		let set = new Set<string>();
+		if (this.G.terminals.has(symbol) || symbol === Misc.EPSILON) {
+            set.add(symbol);
+            return set;
+        }
+        const rightSides = this.G.getRightSidesWith(symbol);
+        for (let rightSide of rightSides) {
+            set = new Set<string>(
+                [...set].concat(...this.first(rightSide))
+            );
+        }
+
+        if (set.has(Misc.EPSILON)) {
+            set.delete(Misc.EPSILON);
+
+            set = new Set<string>([...set].concat(...this.first(symbol)));
+        }
+	}
+	/*
+	private readonly follow = (symbol: string) => {
+        let set = new Set<string>();
+        if (symbol === this.G.startSymbol) {
+            set.add("$");
+        }
+        const gammas = this.G.getLeftSidesAndGammasWith(symbol);
+        for (let leftSide in gammas) {
+            if (gammas[leftSide].length !== 0) {
+                set = new Set<string>(
+                    [...set].concat(...this.first(gammas[leftSide]))
+                );
+                if (set.has(Misc.EPSILON)) {
+                    set.delete(Misc.EPSILON);
+                    set = new Set<string>(
+                        [...set].concat(...this.follow(leftSide))
+                    );
+                }
+            } else {
+                if (leftSide === symbol) {
+                    return new Set<string>();
+                }
+                set = new Set<string>(
+                    [...set].concat(...this.follow(leftSide))
+                );
+            }
+        }
+
+        return set;
+	};
+	
+	*/
 }
 
 export { LR0 };
